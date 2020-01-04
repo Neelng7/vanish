29525223b0f2401b846ba2d5d715b65e292010af
Neelng7 <neelnaraingupta@gmail.com>
2020-01-04 13:50:08 +0530

    d

--- Log.js
+++ Log.js
@@ -4,4 +4,18 @@ class Log extends BaseClass{
     this.image = loadImage("sprites/wood2.png");
     Matter.Body.setAngle(this.body, angle);
   }
+  display(){ 
+    console.log(this.body.speed);
+     if(this.body.speed<4.5){
+        super.display();
+       }
+         else{  
+      World.remove(world, this.body);
+      push();
+      this.Visibility = this.Visibility - 5;
+      tint(255,this.Visibility);
+      image(this.image,this.body.position.x, this.body.position.y,50,50);
+      pop();
+     } 
+    }
 }
